27d4a6d14aa724a9e59a421fc61780da2bde0917
Diego Andrés <diegoandres_cortes@outlook.com>
2020-01-27 18:57:15 -0600

    Initial Release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Reseller.<br /><br />Author: The <a href="https://smftricks.com">SMF Tricks</a> Team';
+
+?>
