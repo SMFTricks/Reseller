470e6d43dc7d65a316cc6203422127d1237f3528
DiegoAndresCortes <diegoandres_cortes@outlook.com>
2024-07-15 01:22:15 +0000

    Update Theme-Customs
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/ThemeCustoms/cover.spanish_es.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package Theme Customs
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2023, SMF Tricks
+ * @license MIT
+ */
+
+$txt['st_category_upload'] = 'Cargar portada';
+$txt['st_category_cover_current'] = '<div class="noticebox smalltext">Subir una nueva portada reemplazará la actual.<br>
+<span class="smalltext">Portada Actual:</span><br>%1$s</div>';
+$txt['st_category_nocover'] = '<div class="noticebox smalltext">Esta categoría no tiene portada.</div>';
+$txt['st_category_cover_clear'] = 'Eliminar portada';
